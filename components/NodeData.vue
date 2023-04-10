4a6b5990b5229788c83f8f92514b5be938754c86
lakshman-500 <lakshman.eranki@500apps.com>
2023-04-10 17:35:56 +0530

    indentation format

--- components/NodeData.vue
+++ components/NodeData.vue
@@ -1,25 +1,25 @@
 <script setup>
-const {flow} = useFlow()
+const { flow } = useFlow();
 
-const emits = defineEmits(["addNode"])
+const emits = defineEmits(["addNode"]);
 
-const addNode = (f)=>{
-    console.log("emit : add ..")
-    emits("addNode", f)
-}
-
+const addNode = (f) => {
+  console.log("emit : add ..");
+  emits("addNode", f);
+};
 </script>
 
-
-
 <template>
-     <div  class="w-full">
-           <p class="drag-drawflow mb-4 border border-blue rounded px-3 py-1"
-           v-for="f in flow" :key="f" 
-           draggable="true"
-             :data-node="n.item" @dragstart="addNode(f)"                        
-             >
-                {{ f.name }}
-            </p>     
-     </div>
+  <div class="w-full">
+    <p
+      class="drag-drawflow mb-4 border border-blue rounded px-3 py-1"
+      v-for="f in flow"
+      :key="f"
+      draggable="true"
+      :data-node="n.item"
+      @dragstart="addNode(f)"
+    >
+      {{ f.name }}
+    </p>
+  </div>
 </template>
