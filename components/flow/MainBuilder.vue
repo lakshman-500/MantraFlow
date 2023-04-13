e1faafb4f19c255ff4fe5c8aed923c06e6198962
lakshman-500 <lakshman.eranki@500apps.com>
2023-04-13 16:30:43 +0530

    tab from  headless

--- components/flow/MainBuilder.vue
+++ components/flow/MainBuilder.vue
@@ -36,56 +36,29 @@
       />
     </div>
     <div class="w-[300px]">
-      <!-- <textarea v-model="dialogData" class="auto-grow" /> -->
-
-      <div>
-        <div class="sm:hidden">
-          <label for="tabs" class="sr-only">Select a tab</label>
-          <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
-          <select
-            id="tabs"
-            name="tabs"
-            class="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
-          >
-            <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">
-              {{ tab.name }}
-            </option>
-          </select>
-        </div>
-        <div class="hidden sm:block">
-          <nav class="flex space-x-4" aria-label="Tabs">
-            <a
-              v-for="tab in tabs"
-              :key="tab.name"
-              :href="tab.href"
-              :class="[
-                tab.current
-                  ? 'bg-gray-100 text-gray-700'
-                  : 'text-gray-500 hover:text-gray-700',
-                'rounded-md px-3 py-2 text-sm font-medium',
-              ]"
-              :aria-current="tab.current ? 'page' : undefined"
-              >{{ tab.name }}</a
-            >
-          </nav>
-          <!-- <div id="#messages">
-            <ul>
-              <li v-for="l in dialogData">{{ l }}</li>
-            </ul>
-          </div> -->
-          <div id="#flowJson">
-            {{ flowData }}
-            <!-- <ul>
-      <li v-for="l in dialogData">{{ l }}</li>
-    </ul> -->
-          </div>
-        </div>
-      </div>
+      <TabGroup>
+        <TabList class="flex gap-4">
+          <Tab><div class="bg-blue p-1">Flow JSON</div></Tab>
+          <Tab><div class="bg-blue p-1">Logs</div></Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel> {{ flowData }}</TabPanel>
+          <TabPanel>
+            <div>
+              <ul>
+                <li v-for="l in dialogData">{{ l }}</li>
+              </ul>
+            </div>
+          </TabPanel>
+          <TabPanel>Content 3</TabPanel>
+        </TabPanels>
+      </TabGroup>
     </div>
   </div>
 </template>
 
 <script setup>
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/vue";
 const { nodeTypes } = useNodeTypes();
 const dialogVisible = ref(false);
 const dialogData = ref([]); //defineProps(["flowData"]);
@@ -142,7 +115,7 @@ function exportEditor() {
 //}
 
 const tabs = [
-  // { name: "Messages", href: "#messages", current: true },
+  { name: "Messages", href: "#messages", current: true },
   { name: "Flow Json.", href: "#flowJson", current: false },
 ];
 </script>
